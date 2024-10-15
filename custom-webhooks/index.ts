a7a4b01aa226665ea1dd3ae68209c65e17f8062d
Bastien <9058670+bmoyroud@users.noreply.github.com>
2024-10-15 21:31:27 +0200

    Log decoded log

--- custom-webhooks/index.ts
+++ custom-webhooks/index.ts
@@ -2,6 +2,8 @@ import { Hono, type Context } from "@hono/hono";
 import { Payload, TestPayload } from "./utils/schemas.ts";
 import { parse, ValiError } from "@valibot/valibot";
 import { TRANSFER_EVENT_SIGNATURE } from "./utils/event-signatures.ts";
+import { parseTransfer } from "./utils/parseTransfer.ts";
+import { DecodeLogDataMismatch } from "npm:viem";
 
 const app = new Hono();
 
@@ -15,18 +17,16 @@ app.post("/", async (c: Context) => {
 
     const logs = payload.event.data.block.logs;
     for (const log of logs) {
-      const [topic0] = log.topics;
-      switch (topic0) {
-        case TRANSFER_EVENT_SIGNATURE: {
-          continue;
-        }
-      }
+      const decodedLog = parseTransfer(log);
+      console.log(decodedLog);
     }
   } catch (e) {
     if (e instanceof SyntaxError) {
       console.log("Invalid JSON");
     } else if (e instanceof ValiError) {
       console.log(e.message);
+    } else if (e instanceof DecodeLogDataMismatch) {
+      console.log(e.message, e.params, e.data, e.abiItem);
     }
   }
   return c.status(200);
