fd18db710e4cb8d03650f616cb368c5574133888
Bastien <9058670+bmoyroud@users.noreply.github.com>
2024-10-15 21:31:16 +0200

    Decode Transfer event logs for ERC20, ER721 or ERC1155

--- custom-webhooks/utils/parseTransfer.ts
+++ custom-webhooks/utils/parseTransfer.ts
@@ -1,3 +1,23 @@
+import { decodeEventLog, parseAbi } from "npm:viem";
 import type { Log } from "./types.ts";
 
-function parseTransfer(log: Log) {}
+export function parseTransfer(log: Log) {
+  const { data, topics } = log;
+  return decodeEventLog({
+    abi: parseAbi([
+      // ERC20
+      "event Transfer(address indexed from, address indexed to, uint256 value)",
+      // "event Approval(address indexed owner, address indexed spender, uint256 value)",
+      // ERC721
+      "event Transfer(address indexed from, address indexed to, uint256 indexed tokenId)",
+      // "event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId)",
+      // "event ApprovalForAll(address indexed owner, address indexed operator, bool approved)",
+      // // ERC1155
+      "event TransferSingle(address indexed operator, address indexed from, address indexed to, uint256 id, uint256 value)",
+      "event TransferBatch(address indexed operator, address indexed from, address indexed to, uint256[] ids, uint256[] values)",
+      // "event ApprovalForAll(address indexed account, address indexed operator, bool approved)",
+    ]),
+    data,
+    topics,
+  });
+}
