1250d243d69d806fb3d9efa454ea36b94c0ec5c0
Bastien <9058670+bmoyroud@users.noreply.github.com>
2024-10-15 22:54:25 +0200

    Convert custom webhook to nft activity webhook (erc1155)

--- custom-webhooks/index.ts
+++ custom-webhooks/index.ts
@@ -3,6 +3,7 @@ import { Payload, TestPayload } from "./utils/schemas.ts";
 import { parse, ValiError } from "@valibot/valibot";
 import { parseTransfer } from "./utils/parseTransfer.ts";
 import { DecodeLogDataMismatch } from "npm:viem";
+import { toHex } from "npm:viem";
 
 const app = new Hono();
 
@@ -14,12 +15,69 @@ app.post("/", async (c: Context) => {
     const body = await c.req.json();
     const payload = parse(Payload, body);
 
-    const logs = payload.event.data.block.logs;
+    const {
+      hash: blockHash,
+      number: blockNumber,
+      logs,
+    } = payload.event.data.block;
     for (const log of logs) {
       const decodedLog = parseTransfer(log);
-      console.log({ address: log.account.address, ...decodedLog });
+
+      const isErc1155 =
+        decodedLog.eventName === "TransferSingle" ||
+        decodedLog.eventName === "TransferBatch";
+
+      if (isErc1155) {
+        const fromAddress = decodedLog.args.from;
+        const toAddress = decodedLog.args.to;
+        const contractAddress = log.account.address;
+        const blockNum = toHex(blockNumber);
+        const hash = log.transaction.hash;
+        const erc1155Metadata =
+          decodedLog.eventName === "TransferBatch"
+            ? decodedLog.args.ids.map((tokenId, i) => ({
+                tokenId: toHex(tokenId),
+                value: toHex(decodedLog.args.values[i]),
+              }))
+            : [
+                {
+                  tokenId: toHex(decodedLog.args.id),
+                  value: toHex(decodedLog.args.value),
+                },
+              ];
+        const category = "erc1155";
+        const log2 = {
+          address: contractAddress,
+          topics: log.topics,
+          data: log.data,
+          blockNumber: toHex(blockNumber),
+          transactionHash: hash,
+          transactionIndex: toHex(log.transaction.index),
+          blockHash,
+          logIndex: toHex(log.index),
+          // removed: null,
+        };
+
+        const activityItem = {
+          fromAddress,
+          toAddress,
+          contractAddress,
+          blockNum,
+          hash,
+          erc1155Metadata,
+          category,
+          log: log2,
+        };
+        const activity =
+          decodedLog.eventName === "TransferBatch"
+            ? new Array(decodedLog.args.ids.length).fill(activityItem)
+            : [activityItem];
+
+        console.log(JSON.stringify(activity, null, 2));
+      }
     }
   } catch (e) {
+    console.log(e);
     if (e instanceof SyntaxError) {
       console.log("Invalid JSON");
     } else if (e instanceof ValiError) {
