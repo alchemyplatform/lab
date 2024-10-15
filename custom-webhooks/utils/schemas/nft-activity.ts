ba6e06f5176693ec5abe612db780071c28476113
Bastien <9058670+bmoyroud@users.noreply.github.com>
2024-10-15 22:53:52 +0200

    Add example nft-activity webhook

---
+++ custom-webhooks/utils/schemas/nft-activity.ts
@@ -0,0 +1,43 @@
+const json = {
+  webhookId: "wh_yq51m67bytws4rk8",
+  id: "whevt_yp2fw69pdk3mo1tf",
+  createdAt: "2024-10-15T19:47:13.758Z",
+  type: "NFT_ACTIVITY",
+  event: {
+    network: "ETH_SEPOLIA",
+    activity: [
+      {
+        fromAddress: "0x0000000000000000000000000000000000000000",
+        toAddress: "0x93f9ecdaf22f8b60bc24641dd5ed43ca92405cb7",
+        contractAddress: "0xe4a99f34be5f749aad26cbcfd6b9213ac8bc0589",
+        blockNum: "0x690335",
+        hash: "0x0156d50e23600c1a256dc5a8a9783459fc1e40df2b22fe3168503d0c7ffe3be4",
+        erc1155Metadata: [
+          {
+            tokenId: "0x0",
+            value: "0xa",
+          },
+        ],
+        category: "erc1155",
+        log: {
+          address: "0xe4a99f34be5f749aad26cbcfd6b9213ac8bc0589",
+          topics: [
+            "0xc3d58168c5ae7397731d063d5bbf3d657854427343f4c083240f7aacaa2d0f62",
+            "0x00000000000000000000000016c3a8563631dec0e3360c6a6bffc190ec41b99f",
+            "0x0000000000000000000000000000000000000000000000000000000000000000",
+            "0x00000000000000000000000093f9ecdaf22f8b60bc24641dd5ed43ca92405cb7",
+          ],
+          data: "0x0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000a",
+          blockNumber: "0x690335",
+          transactionHash:
+            "0x0156d50e23600c1a256dc5a8a9783459fc1e40df2b22fe3168503d0c7ffe3be4",
+          transactionIndex: "0x4f",
+          blockHash:
+            "0xc3a8e0065d564137509d174784733e071937a3219dbbb6f3d191c00d68b2b4c1",
+          logIndex: "0x65",
+          removed: false,
+        },
+      },
+    ],
+  },
+};
