c8255b4020e19e384e6aee26c323803abf26c312
Bastien <9058670+bmoyroud@users.noreply.github.com>
2024-10-16 11:57:48 +0200

    Move create-webhook.ts to scripts

--- custom-webhooks/scripts/create-webhook.ts
+++ custom-webhooks/scripts/create-webhook.ts
@@ -6,11 +6,11 @@ const response = await fetch(url, {
   method: "POST",
   headers: {
     "Content-Type": "application/json",
-    "X-Alchemy-Token": "",
+    "X-Alchemy-Token": Deno.env.get("X_ALCHEMY_TOKEN")!,
   },
   body: JSON.stringify({
     webhook_url: webhookUrl,
-    network: "ETH_MAINNET",
+    network: "ETH_SEPOLIA",
     webhook_type: "GRAPHQL",
     graphql_query: {
       skip_empty_messages: true,
@@ -20,10 +20,17 @@ const response = await fetch(url, {
           hash
           logs(
             filter: {
+              addresses: [
+                # ERC721 - Contract we deployed
+                "0x07c39105a9BD23dA07d728B7d5D7b8B21137A634",
+                
+                # ERC1155 - Contract we deployed
+                "0xe4a99f34be5f749aad26cbcfd6b9213ac8bc0589",
+              ],
               topics: [
                 [
                   # ERC721 - Transfer 	
-                  #"0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef",
+                  "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef",
 
                   # ERC1155 - TransferSingle
                   "0xc3d58168c5ae7397731d063d5bbf3d657854427343f4c083240f7aacaa2d0f62",
