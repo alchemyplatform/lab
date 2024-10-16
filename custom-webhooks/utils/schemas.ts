f2b820501fa6845a936c81fd5fa7c37dd1d2ccc2
Bastien <9058670+bmoyroud@users.noreply.github.com>
2024-10-16 20:02:26 +0200

    Refactor common schemas into shared.ts

--- custom-webhooks/utils/schemas.ts
+++ custom-webhooks/utils/schemas.ts
@@ -1,17 +1,8 @@
-import { isoTimestamp, literal, number, pipe, string } from "@valibot/valibot";
+import { literal, number, string } from "@valibot/valibot";
 import { array } from "@valibot/valibot";
-import { hexadecimal } from "@valibot/valibot";
-import { length } from "@valibot/valibot";
 import { null_ } from "@valibot/valibot";
 import { strictObject } from "@valibot/valibot";
-import { startsWith } from "@valibot/valibot";
-
-const Hex = pipe(
-  string(),
-  hexadecimal("The value is not a hexadecimal string.")
-);
-
-const Address = pipe(Hex, length(42, "The address is not 42 characters long."));
+import { Address, Hex, Id, WebhookId } from "./schemas/shared.ts";
 
 export const Log = strictObject({
   account: strictObject({
@@ -26,25 +17,6 @@ export const Log = strictObject({
     index: number(),
   }),
 });
-
-// eg. wh_jjcyyktgh9m8x3hd
-const WebhookId = pipe(
-  string(),
-  startsWith("wh_", "The webhook ID does not start with 'wh_'."),
-  length(19, "The webhook ID is not 18 characters long.")
-);
-
-// eg. whevt_dfi0wvt7nrzfpbzr
-const Id = pipe(
-  string(),
-  startsWith("whevt_", "The ID does not start with 'whevt_'."),
-  length(22, "The ID is not 16 characters long.")
-);
-
-const IsoTimestamp = pipe(
-  string(),
-  isoTimestamp("The timestamp is badly formatted.")
-);
 
 const WebhookType = literal("GRAPHQL");
 
---
+++ custom-webhooks/utils/schemas/shared.ts
@@ -0,0 +1,37 @@
+import {
+  hexadecimal,
+  isoTimestamp,
+  length,
+  pipe,
+  startsWith,
+  string,
+} from "@valibot/valibot";
+
+export const Hex = pipe(
+  string(),
+  hexadecimal("The value is not a hexadecimal string.")
+);
+
+export const Address = pipe(
+  Hex,
+  length(42, "The address is not 42 characters long.")
+);
+
+// eg. wh_jjcyyktgh9m8x3hd
+export const WebhookId = pipe(
+  string(),
+  startsWith("wh_", "The webhook ID does not start with 'wh_'."),
+  length(19, "The webhook ID is not 18 characters long.")
+);
+
+// eg. whevt_dfi0wvt7nrzfpbzr
+export const Id = pipe(
+  string(),
+  startsWith("whevt_", "The ID does not start with 'whevt_'."),
+  length(22, "The ID is not 16 characters long.")
+);
+
+export const IsoTimestamp = pipe(
+  string(),
+  isoTimestamp("The timestamp is badly formatted.")
+);
