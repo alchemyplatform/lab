bb7ecd57296c9af3b6f855cebf647095ea4b299f
Bastien <9058670+bmoyroud@users.noreply.github.com>
2024-10-16 22:46:21 +0200

    Add Integer, Hash and Network schemas

--- custom-webhooks/utils/schemas/shared.ts
+++ custom-webhooks/utils/schemas/shared.ts
@@ -1,15 +1,24 @@
 import {
   hexadecimal,
+  integer,
   isoTimestamp,
   length,
+  number,
   pipe,
   startsWith,
   string,
 } from "@valibot/valibot";
 
+export const Integer = pipe(number(), integer());
+
 export const Hex = pipe(
   string(),
   hexadecimal("The value is not a hexadecimal string.")
+);
+
+export const Hash = pipe(
+  Hex,
+  length(66, "The hash is not 66 characters long.")
 );
 
 export const Address = pipe(
@@ -35,3 +44,5 @@ export const IsoTimestamp = pipe(
   string(),
   isoTimestamp("The timestamp is badly formatted.")
 );
+
+export const Network = string();
