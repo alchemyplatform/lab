71c4aea0bd80f555e67dd53e52e6677884f702c9
Bastien <9058670+bmoyroud@users.noreply.github.com>
2024-10-18 01:17:15 +0200

    Add GraphQlToNftActivitySchema to check required fields

---
+++ custom-webhooks/utils/schemas/convert.ts
@@ -0,0 +1,45 @@
+import { array, literal, object, strictObject, union } from "@valibot/valibot";
+import {
+  Hash,
+  Hex,
+  Id,
+  Integer,
+  IsoTimestamp,
+  Network,
+  Topics,
+  WebhookId,
+} from "./shared.ts";
+import { Account, SequenceNumber } from "./custom.ts";
+
+// Required fields to go from Custom webhook -> NFT Activity webhook
+export const GraphQlToNftActivitySchema = strictObject(
+  {
+    webhookId: WebhookId,
+    id: Id,
+    createdAt: IsoTimestamp,
+    type: literal("GRAPHQL"),
+    event: strictObject({
+      data: strictObject({
+        block: object({
+          number: Integer,
+          hash: Hash,
+          logs: array(
+            object({
+              index: Integer,
+              account: Account,
+              topics: Topics,
+              data: union([Hex, literal("0x")]),
+              transaction: object({
+                hash: Hash,
+                index: Integer,
+              }),
+            })
+          ),
+        }),
+      }),
+      sequenceNumber: SequenceNumber,
+      network: Network,
+    }),
+  },
+  "Required fields to go from Custom to NFT Activity webhook are missing."
+);
