d08714b94809d1b3bfffce78f500cf9d62c5ee69
Bastien <9058670+bmoyroud@users.noreply.github.com>
2024-10-18 21:08:23 +0200

    Add latest schemas to AlchemyPayloadSchema

--- webhooks/utils/schemas/index.ts
+++ webhooks/utils/schemas/index.ts
@@ -2,6 +2,9 @@ import { variant, type InferOutput } from "@valibot/valibot";
 import { GraphQlSchema } from "./custom.ts";
 import { AddressActivitySchema } from "./address-activity.ts";
 import { NftActivitySchema } from "./nft-activity.ts";
+import { NftMetadataUpdateSchema } from "./nft-metadata-update.ts";
+import { MinedTransactionSchema } from "./mined-transaction.ts";
+import { DroppedTransactionSchema } from "./dropped-transaction.ts";
 
 export type AlchemyPayload = InferOutput<typeof AlchemyPayloadSchema>;
 
@@ -9,4 +12,7 @@ export const AlchemyPayloadSchema = variant("type", [
   GraphQlSchema,
   AddressActivitySchema,
   NftActivitySchema,
+  NftMetadataUpdateSchema,
+  MinedTransactionSchema,
+  DroppedTransactionSchema,
 ]);
