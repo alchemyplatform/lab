6811cace037c56c446230ed5032b59bc15187dc0
Bastien <9058670+bmoyroud@users.noreply.github.com>
2024-10-18 16:52:00 +0200

    Comment out error handling

--- custom-webhooks/index.ts
+++ custom-webhooks/index.ts
@@ -11,29 +11,29 @@ const app = new Hono<{ Variables: { payload: AlchemyPayload } }>();
 app.post(
   "/",
   validateSignature({
-    signingKey: "whsec_QkKHJwIh9G8OdsrKllkGpA60",
+    signingKey: "whsec_P4Onft0WFk2FICAH6jXXLGe4",
     // signingKey: new Map([
     //   ["wh_zkx600u6a74ntw19", "whsec_hye014cZDVAnzQEvZ2Qa45eZ"],
     //   ["wh_yj2nnzb6dxontpm2", "whsec_7hmAMzxbN2YKBlp4H5F3CXg7"],
     // ]),
   }),
   validatePayload,
-  // convertToNftActivity,
-  superWebhook,
-  async (ctx) => {
-    let json;
-    try {
-      json = await ctx.req.json();
-      const payload = ctx.var.payload;
-      // console.log("Payload", payload);
-    } catch (e) {
-      if (e instanceof ValiError) {
-        console.log(
-          e.issues.map((i) => i.path.map((path) => path.key).join("."))
-        );
-      }
-      console.log(JSON.stringify(json.logs, null, 2));
-    }
+  convertToNftActivity,
+  // superWebhook,
+  (ctx) => {
+    // let json;
+    // try {
+    //   json = await ctx.req.json();
+    //   const payload = ctx.var.payload;
+    //   // console.log("Payload", payload);
+    // } catch (e) {
+    //   if (e instanceof ValiError) {
+    //     console.log(
+    //       e.issues.map((i) => i.path.map((path) => path.key).join("."))
+    //     );
+    //   }
+    //   console.log(JSON.stringify(json.logs, null, 2));
+    // }
     return new Response("", { status: 200 });
   }
 );
