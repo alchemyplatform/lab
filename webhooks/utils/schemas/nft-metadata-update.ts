703dc7bd87cd86e11abeecb43c5a43d46c418d06
Bastien <9058670+bmoyroud@users.noreply.github.com>
2024-10-18 21:07:22 +0200

    Add Event2

--- webhooks/utils/schemas/nft-metadata-update.ts
+++ webhooks/utils/schemas/nft-metadata-update.ts
@@ -1,25 +1,70 @@
 import {
+  array,
+  boolean,
   literal,
   number,
-  pipe,
   strictObject,
   string,
-  url,
+  union,
 } from "@valibot/valibot";
-import { Address, Id, IsoTimestamp, Network, WebhookId } from "./shared.ts";
+import {
+  Address,
+  Id,
+  IsoTimestamp,
+  Network,
+  Url,
+  WebhookId,
+} from "./shared.ts";
+
+const BaseEvent = strictObject({
+  network: Network,
+  contractAddress: Address,
+  tokenId: string(),
+  networkId: number(),
+  metadataUri: Url,
+  updatedAt: IsoTimestamp,
+});
+
+const Event1 = strictObject({
+  ...BaseEvent.entries,
+  rawMetadata: string(),
+});
+
+const Event2 = strictObject({
+  ...BaseEvent.entries,
+  name: string(),
+  description: string(),
+  imageUri: Url,
+  attributes: array(
+    strictObject({
+      traitType: string(),
+      value: string(),
+    })
+  ),
+  rawMetadata: strictObject({
+    background_image: Url,
+    image: Url,
+    last_request_date: number(),
+    is_normalized: boolean(),
+    image_url: Url,
+    name: string(),
+    description: string(),
+    attributes: array(
+      strictObject({
+        display_type: string(),
+        value: union([string(), number()]),
+        trait_type: string(),
+      })
+    ),
+    version: number(),
+    url: Url,
+  }),
+});
 
 export const NftMetadataUpdateSchema = strictObject({
   webhookId: WebhookId,
   id: Id,
   createdAt: IsoTimestamp,
   type: literal("NFT_METADATA_UPDATE"),
-  event: strictObject({
-    network: Network,
-    contractAddress: Address,
-    tokenId: string(),
-    networkId: number(),
-    metadataUri: pipe(string(), url()),
-    updatedAt: IsoTimestamp,
-    rawMetadata: string(),
-  }),
+  event: union([Event1, Event2]),
 });
