19c22da7ece35dc4a6f5d781102581e3b66e187a
Bastien <9058670+bmoyroud@users.noreply.github.com>
2024-10-18 16:39:35 +0200

    Add GraphQlToAddressActivitySchema to validate required fields to go from Custom to Address Activity webhook

--- custom-webhooks/utils/schemas/convert.ts
+++ custom-webhooks/utils/schemas/convert.ts
@@ -1,4 +1,11 @@
-import { array, literal, object, strictObject, union } from "@valibot/valibot";
+import {
+  array,
+  literal,
+  object,
+  strictObject,
+  union,
+  type InferOutput,
+} from "@valibot/valibot";
 import {
   Hash,
   Hex,
@@ -42,4 +49,37 @@ export const GraphQlToNftActivitySchema = strictObject(
     }),
   },
   "Required fields to go from Custom to NFT Activity webhook are missing."
+);
+
+// Required fields to go from Custom webhook -> Address Activity webhook
+export const GraphQlToAddressActivitySchema = strictObject(
+  {
+    webhookId: WebhookId,
+    id: Id,
+    createdAt: IsoTimestamp,
+    type: literal("GRAPHQL"),
+    event: strictObject({
+      data: strictObject({
+        block: object({
+          number: Integer,
+          hash: Hash,
+          logs: array(
+            object({
+              index: Integer,
+              account: Account,
+              topics: Topics,
+              data: union([Hex, literal("0x")]),
+              transaction: object({
+                hash: Hash,
+                index: Integer,
+              }),
+            })
+          ),
+        }),
+      }),
+      sequenceNumber: SequenceNumber,
+      network: Network,
+    }),
+  },
+  "Required fields to go from Custom to Address Activity webhook are missing."
 );
