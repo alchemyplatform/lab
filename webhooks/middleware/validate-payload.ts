79c3673071d996d0a84e086e9cc443cccf46838a
Bastien <9058670+bmoyroud@users.noreply.github.com>
2024-10-21 10:35:12 +0200

    Add clearer errors

--- webhooks/middleware/validate-payload.ts
+++ webhooks/middleware/validate-payload.ts
@@ -14,12 +14,26 @@ export const validatePayload = createMiddleware<{
     ctx.set("payload", payload);
     await next();
   } catch (e) {
+    if (e instanceof SyntaxError) {
+      const errMessage = `Error parsing payload: ${e.message}`;
+      return ctx.json({ error: errMessage }, { status: 400 });
+    }
+
     if (e instanceof ValiError) {
       console.log(e.message);
-      console.log(
-        e.issues.map((i) => i.path.map((path) => path.key).join("."))
+      const errMessage = `Error validating payload: ${e.message}`;
+      const issuePath = e.issues.map((i) =>
+        i.path.map((path) => path.key).join(".")
+      );
+      return ctx.json(
+        { error: errMessage, issues: issuePath },
+        { status: 400 }
       );
     }
-    return ctx.json({ error: "Invalid payload" }, { status: 400 });
+
+    return ctx.json(
+      { error: "Error in validatePayload middleware" },
+      { status: 400 }
+    );
   }
 });
