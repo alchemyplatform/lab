214216d394e96a69bd6928cb41cc502a2ccacbed
Bastien <9058670+bmoyroud@users.noreply.github.com>
2024-10-22 17:08:58 +0200

    Refactor Webhook schema

--- webhooks/utils/schemas/api/get-all-webhooks.ts
+++ webhooks/utils/schemas/api/get-all-webhooks.ts
@@ -3,27 +3,9 @@
  *
  * Docs - https://docs.alchemy.com/reference/team-webhooks
  */
-import { array, boolean, optional, strictObject } from "@valibot/valibot";
-import {
-  Integer,
-  WebhookId,
-  WebhookSigningKey,
-  WebhookType,
-  WebhookVersion,
-} from "../shared.ts";
-import { Network } from "../shared.ts";
-import { Url } from "../shared.ts";
 
-const Webhook = strictObject({
-  id: WebhookId,
-  network: Network,
-  webhook_type: optional(WebhookType),
-  webhook_url: Url,
-  is_active: boolean(),
-  time_created: Integer,
-  signing_key: WebhookSigningKey,
-  version: WebhookVersion,
-});
+import { array, strictObject } from "@valibot/valibot";
+import { Webhook } from "./shared.ts";
 
 export const ResponseGetAllWebhooks = strictObject({
   data: array(Webhook),
---
+++ webhooks/utils/schemas/api/shared.ts
@@ -0,0 +1,21 @@
+import { boolean, optional, strictObject } from "@valibot/valibot";
+import {
+  Integer,
+  WebhookId,
+  WebhookSigningKey,
+  WebhookType,
+  WebhookVersion,
+} from "../shared.ts";
+import { Network } from "../shared.ts";
+import { Url } from "../shared.ts";
+
+export const Webhook = strictObject({
+  id: WebhookId,
+  network: Network,
+  webhook_type: optional(WebhookType),
+  webhook_url: Url,
+  is_active: boolean(),
+  time_created: Integer,
+  signing_key: WebhookSigningKey,
+  version: WebhookVersion,
+});
