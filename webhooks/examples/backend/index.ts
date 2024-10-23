34684c5587c622137032bcf9a24de6effbf0d039
Bastien <9058670+bmoyroud@users.noreply.github.com>
2024-10-23 15:02:28 +0200

    Update signing key

--- webhooks/examples/backend/index.ts
+++ webhooks/examples/backend/index.ts
@@ -1,3 +1,4 @@
+// deno run --allow-net --allow-env --env --watch examples/backend/index.ts
 import { Hono } from "@hono/hono";
 import { validateSignature } from "../../middleware/validate-signature.ts";
 import { validatePayload } from "../../middleware/validate-payload.ts";
@@ -9,7 +10,7 @@ const app = new Hono<{ Variables: { payload: AlchemyPayload } }>();
 app.post(
   "/",
   validateSignature({
-    signingKey: "whsec_Pd0W8f2mMnXv2YbpCiB8DzlY",
+    signingKey: "whsec_pAX4hIxJksTkD3wBDG0TnQX4",
     // signingKey: new Map([
     //   ["wh_zkx600u6a74ntw19", "whsec_hye014cZDVAnzQEvZ2Qa45eZ"],
     //   ["wh_yj2nnzb6dxontpm2", "whsec_7hmAMzxbN2YKBlp4H5F3CXg7"],
