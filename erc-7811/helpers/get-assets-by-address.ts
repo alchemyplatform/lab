d52f81dbdac6e90f704f03dff37d528c98fd8904
bastien <9058670+bmoyroud@users.noreply.github.com>
2025-08-06 01:27:38 +0200

    chore: add comment

--- erc-7811/helpers/get-assets-by-address.ts
+++ erc-7811/helpers/get-assets-by-address.ts
@@ -1,6 +1,7 @@
 import { getNftsByAddress } from "./get-nfts-by-address";
 import { getTokensByAddress } from "./get-tokens-by-address";
 
+// Get all assets for an address
 async function getAssetsByAddress({ address, networks }: { address: string, networks: string[] }) {
   const tokens = await getTokensByAddress({ address, networks });
   const nfts = await getNftsByAddress({ address, networks });
