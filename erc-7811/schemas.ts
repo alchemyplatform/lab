16da2912fdf7a6b86758dd1ec9dda01eb5f7d34f
bastien <9058670+bmoyroud@users.noreply.github.com>
2025-08-05 22:05:46 +0200

    feat: add valibot schemas for erc-7811 types

---
+++ erc-7811/schemas.ts
@@ -0,0 +1,106 @@
+import {
+  pipe,
+  string,
+  hexadecimal,
+  length,
+  literal,
+  union,
+  object,
+  optional,
+  record,
+  array,
+  any,
+  number,
+  integer,
+} from "valibot";
+
+
+const Hex = pipe(
+  string(),
+  hexadecimal("The value is not a hexadecimal string.")
+);
+
+// const Hash = pipe(
+//   Hex,
+//   length(66, "The hash is not 66 characters long.")
+// );
+
+const Address = pipe(
+  Hex,
+  length(42, "The address is not 42 characters long.")
+);
+
+const AssetType = union([
+  literal("native"),
+  literal("erc20"),
+  literal("erc721"),
+  string()
+])
+
+const AddressOrNative = union([
+  Address,
+  literal("native")
+])
+
+const Eip155ChainId = Hex;
+
+const AssetFilter = record(Eip155ChainId, array(object({
+  address: AddressOrNative,
+  type: AssetType
+})));
+
+const AssetTypeFilter = array(AssetType);
+
+const WalletGetAssetsRequest = object({
+  account: Address,
+  assetFilter: optional(AssetFilter),
+  assetTypeFilter: optional(AssetTypeFilter),
+  chainFilter: optional(array(Eip155ChainId))
+})
+
+// const Asset = object({
+//   address: AddressOrNative,
+//   balance: Hex,
+//   type: AssetType,
+//   metadata: any(),
+// });
+
+const NativeAsset = object({
+  address: literal("native"),
+  balance: Hex,
+  type: literal("native"),
+});
+
+const Erc20Asset = object({
+  address: Address,
+  balance: Hex,
+  type: literal("erc20"),
+  metadata: object({
+    name: string(),
+    symbol: string(),
+    decimals: pipe(number(), integer()),
+  })
+});
+
+const Erc721Asset = object({
+  address: Address,
+  balance: Hex,
+  type: literal("erc721"),
+  metadata: object({
+    name: string(),
+    symbol: string(),
+    tokenId: Hex,
+    tokenURI: optional(string()),
+  })
+});
+
+const GenericAsset = object({
+  address: Address,
+  balance: Hex,
+  type: string(),
+  metadata: any(),
+});
+
+const Asset = union([NativeAsset, Erc20Asset, Erc721Asset, GenericAsset]);
+
+const WalletGetAssetsResponse = record(Eip155ChainId, array(Asset));
