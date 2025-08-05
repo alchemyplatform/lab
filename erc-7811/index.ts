4b34e7dda602740d53c753732880dfc23f87f59d
bastien <9058670+bmoyroud@users.noreply.github.com>
2025-08-05 23:13:11 +0200

    chore: map chain ids to networks

--- erc-7811/index.ts
+++ erc-7811/index.ts
@@ -1,10 +1,29 @@
 import { parse } from "valibot";
 import { WalletGetAssetsRequest, type WalletGetAssetsResponse } from "./schemas";
+import { getTokenBalancesByAddress } from "./helpers/get-token-balances-by-address";
+import { getNftsByAddress } from "./helpers/get-nfts-by-address";
+import { getAssetsByAddress } from "./helpers/get-assets-by-address";
 
 
 const chainIdToNetwork = new Map<string, string>([
-  ["0x1", "ethereum-mainnet"],
-  ["0xa", "sepolia"],
+  ["0x1", "eth-mainnet"],
+  ["0xaa36a7", "eth-sepolia"],
+  ["0xa4b1", "arb-mainnet"],
+  ["0x66eee", "arb-sepolia"],
+  ["0x2105", "base-mainnet"],
+  ["0x14a33", "base-sepolia"],
+  ["0x38", "bnb-mainnet"],
+  ["0x61", "bnb-testnet"],
+  ["0xa", "opt-mainnet"],
+  ["0x1a4", "opt-sepolia"],
+  ["0x89", "polygon-mainnet"],
+  ["0x13882", "polygon-amoy"],
+  ["0x144", "zksync-mainnet"],
+  ["0x12c", "zksync-sepolia"],
+  ["0xa86a", "avax-mainnet"],
+  ["0xa869", "avax-fuji"],
+  ["0x1389", "sonic-mainnet"],
+  ["0x138a", "sonic-blaze"],
 ]);
 
 async function getAssets(request: WalletGetAssetsRequest): Promise<WalletGetAssetsResponse> {
@@ -23,26 +42,48 @@ See https://eip.tools/eip/7811 for more details.`);
   if (assetFilter) {
     for (const [chainId, assets] of Object.entries(assetFilter)) {
       console.log(chainId, assets);
+
+      const network = chainIdToNetwork.get(chainId);
+      if (!network) {
+        throw new Error(`Portfolio API does not support chainId: ${chainId}.`);
+      }
     }
+
+    return {
+      "0x1": [
+        {
+          "address": "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+          "balance": "0xcaaea35047fe5702",
+          "type": "ERC20",
+          "metadata": {
+            "name": "Token",
+            "symbol": "TOK",
+            "decimals": 18
+          }
+        },
+      ]
+    };
   }
 
-  return {
-    "0x1": [
-      {
-        "address": "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
-        "balance": "0xcaaea35047fe5702",
-        "type": "ERC20",
-        "metadata": {
-          "name": "Token",
-          "symbol": "TOK",
-          "decimals": 18
-        }
-      },
-    ]
+  /*
+    assetTypeFilter is an OPTIONAL field that specifies an array of asset types, as defined in this ERC. If assetTypeFilter field is provided, wallet MUST include only assets with those types in the response.
+  */
+  if (assetTypeFilter) {
+
   }
+
+  /*
+    chainFilter is an OPTIONAL field that specifies an array of chain ids, where each value in the array MUST be a valid EIP-155 chainId.
+  */
+  if (chainFilter) {
+
+  }
+
+  /*
+    If the assetFilter field is omitted, the wallet SHOULD return all available assets for the requested account. It is RECOMMENDED that the returned assets be ordered by estimated value in descending order, as determined by the wallet.
+  */
+  const assets = await getAssetsByAddress({ address: account, networks: ['eth-mainnet'] });
 }
-
-
 
 
 // vitalik.eth
@@ -50,24 +91,24 @@ const account = "0xd8da6bf26964af9d7eed9e03e53415d37aa96045";
 
 const results = await getAssets({
   "account": "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045",
-  "assetFilter": {
-    "0x1": [
-      {
-        "address": "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045",
-        "type": "erc20"
-      },
-      {
-        "address": "native",
-        "type": "native"
-      }
-    ],
-    "0xa": [
-      {
-        "address": "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045",
-        "type": "erc20"
-      }
-    ]
-  },
+  // "assetFilter": {
+  //   "0x1": [
+  //     {
+  //       "address": "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045",
+  //       "type": "erc20"
+  //     },
+  //     {
+  //       "address": "native",
+  //       "type": "native"
+  //     }
+  //   ],
+  //   "0xa": [
+  //     {
+  //       "address": "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045",
+  //       "type": "erc20"
+  //     }
+  //   ]
+  // },
   // "assetTypeFilter": ["erc20", "native"],
   // "chainFilter": ["0x1"]
 });
