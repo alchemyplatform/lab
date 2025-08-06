0a2b88da43fd7d555a34cd63060151e3489c9516
bastien <9058670+bmoyroud@users.noreply.github.com>
2025-08-07 00:15:52 +0200

    chore: add tokens table & one-to-many relationships

--- ponder/uniswap-v4/ponder.schema.ts
+++ ponder/uniswap-v4/ponder.schema.ts
@@ -1,4 +1,4 @@
-import { onchainTable } from "ponder";
+import { onchainTable, relations } from "ponder";
 
 export const poolManagers = onchainTable("pool_managers", (t) => ({
   // poolManager address
@@ -59,4 +59,65 @@ export const pools = onchainTable("pools", (t) => ({
 
   // block pool was created at
   createdAtBlockNumber: t.bigint().notNull(),
+}));
+
+export const poolsRelations = relations(pools, ({ one }) => ({
+  token0: one(tokens),
+  token1: one(tokens),
+}));
+
+export const tokens = onchainTable("tokens", (t) => ({
+  //  token address
+  id: t.text().primaryKey(),
+
+  // token symbol
+  symbol: t.text(),
+
+  // token name
+  name: t.text(),
+
+  // token decimals
+  decimals: t.bigint(),
+
+  // token total supply
+  totalSupply: t.bigint(),
+
+  // volume in token units
+  volume: t.doublePrecision(),
+
+  // volume in derived USD
+  volumeUSD: t.doublePrecision(),
+
+  // volume in USD even on pools with less reliable USD values
+  untrackedVolumeUSD: t.doublePrecision(),
+
+  // fees in USD
+  feesUSD: t.doublePrecision(),
+
+  // transactions across all pools that include this token
+  txCount: t.bigint(),
+
+  // TODO: use relation instead?
+  // number of pools containing this token
+  poolCount: t.bigint(),
+
+  // liquidity across all pools in token units
+  totalValueLocked: t.doublePrecision(),
+
+  // liquidity across all pools in derived USD
+  totalValueLockedUSD: t.doublePrecision(),
+
+  // TVL derived in USD untracked
+  totalValueLockedUSDUntracked: t.doublePrecision(),
+
+  // Note: for chains where ETH is not the native token, this will be the derived price of that chain's native token, effectively, this should be renamed derivedNative.
+  derivedETH: t.doublePrecision(),
+
+}));
+
+export const tokensRelations = relations(tokens, ({ many }) => ({
+  pools: many(pools),
+
+  // pools token is in that are white listed for USD pricing
+  whitelistPools: many(pools),
 }));
