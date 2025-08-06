d7cece2e3d3aa9d8a5b54a0fc77c4a4a6290374d
bastien <9058670+bmoyroud@users.noreply.github.com>
2025-08-06 22:48:57 +0200

    chore: add separator for block number

--- ponder/uniswap-v4/ponder.config.ts
+++ ponder/uniswap-v4/ponder.config.ts
@@ -10,13 +10,13 @@ export default createConfig({
       chain: "base",
       address: "0x498581fF718922c3f8e6A244956aF099B2652b2b",
       abi: PoolManagerAbi,
-      startBlock: 25350988,
+      startBlock: 25_350_988,
     },
     PositionManager: {
       chain: "base",
       address: "0x7C5f5A4bBd8fD63184577525326123B519429bDc",
       abi: PositionManagerAbi,
-      startBlock: 25350993,
+      startBlock: 25_350_993,
     },
   },
 });
