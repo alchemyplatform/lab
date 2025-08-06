3bef31ed8aeb8a61d5d42daa948853d36db01e3c
bastien <9058670+bmoyroud@users.noreply.github.com>
2025-08-06 23:08:23 +0200

    chore: create pool_managers table

--- ponder/uniswap-v4/ponder.schema.ts
+++ ponder/uniswap-v4/ponder.schema.ts
@@ -1,6 +1,43 @@
-import { onchainTable } from "ponder";
+import { onchainTable, time } from "ponder";
 
-export const example = onchainTable("example", (t) => ({
+export const poolManagers = onchainTable("pool_managers", (t) => ({
+  // poolManager address
   id: t.text().primaryKey(),
-  name: t.text(),
+
+  // amount of pools created
+  poolCount: t.bigint(),
+
+  // amount of transactions all time
+  txCount: t.bigint(),
+
+  // total volume all time in derived USD
+  totalVolumeUSD: t.doublePrecision(),
+
+  // total volume all time in derived ETH
+  totalVolumeETH: t.doublePrecision(),
+
+  // total swap fees all time in USD
+  totalFeesUSD: t.doublePrecision(),
+
+  // total swap fees all time in USD
+  totalFeesETH: t.doublePrecision(),
+
+  // all volume even through less reliable USD values
+  untrackedVolumeUSD: t.doublePrecision(),
+
+  // TVL derived in USD
+  totalValueLockedUSD: t.doublePrecision(),
+
+  // TVL derived in ETH
+  totalValueLockedETH: t.doublePrecision(),
+
+  // TVL derived in USD untracked
+  totalValueLockedUSDUntracked: t.doublePrecision(),
+
+  // TVL derived in ETH untracked
+  totalValueLockedETHUntracked: t.doublePrecision(),
+
+  // current owner of the factory
+  // TODO: create Owner entity?
+  owner: t.text(),
 }));
