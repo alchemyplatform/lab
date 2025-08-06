ef849269d87239e41b1692c0e74657b3531011d8
bastien <9058670+bmoyroud@users.noreply.github.com>
2025-08-06 21:46:42 +0200

    chore: add includeNative and includeErc20 filters

--- erc-7811/helpers/get-tokens-by-address.ts
+++ erc-7811/helpers/get-tokens-by-address.ts
@@ -1,14 +1,23 @@
 import { parse } from "valibot";
 import { GetTokensByAddressRequest, GetTokensByAddressResponse } from "../schemas/portfolio/get-tokens-by-address";
 
-async function getTokensByAddress({ address, networks }: { address: string, networks: string[] }) {
+// TODO: add pagination
+async function getTokensByAddress({ address, networks, includeNative, includeErc20 }: {
+  address: string,
+  networks: string[],
+  includeNative?: boolean,
+  includeErc20?: boolean,
+}): Promise<GetTokensByAddressResponse['data']['tokens']> {
   const request = parse(GetTokensByAddressRequest, {
     addresses: [
       {
         address,
-        networks
+        networks,
       }
     ],
+    includeNativeTokens: includeNative,
+    includeErc20Tokens: includeErc20,
+    withMetadata: true,
   });
   const key = process.env.ALCHEMY_API_KEY;
   const url = `https://api.g.alchemy.com/data/v1/${key}/assets/tokens/by-address`;
