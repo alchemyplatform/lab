bad29cbc43fc9fec1377990476a973e3d04fa03f
bastien <9058670+bmoyroud@users.noreply.github.com>
2025-08-06 22:49:34 +0200

    chore: add listeners for PoolManager events we care about

--- ponder/uniswap-v4/src/PoolManager.ts
+++ ponder/uniswap-v4/src/PoolManager.ts
@@ -1,17 +1,13 @@
 import { ponder } from "ponder:registry";
 
-ponder.on("PoolManager:Approval", async ({ event, context }) => {
-  console.log(event.args);
-});
-
-ponder.on("PoolManager:Donate", async ({ event, context }) => {
-  console.log(event.args);
-});
-
 ponder.on("PoolManager:Initialize", async ({ event, context }) => {
   console.log(event.args);
 });
 
 ponder.on("PoolManager:ModifyLiquidity", async ({ event, context }) => {
+  console.log(event.args);
+});
+
+ponder.on("PoolManager:Swap", async ({ event, context }) => {
   console.log(event.args);
 });
