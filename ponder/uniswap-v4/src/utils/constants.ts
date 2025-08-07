b98f89bb94a42ed419f99a6f7529b5e7e0380d95
bastien <9058670+bmoyroud@users.noreply.github.com>
2025-08-07 10:48:43 +0200

    fix: update decimal constants

--- ponder/uniswap-v4/src/utils/constants.ts
+++ ponder/uniswap-v4/src/utils/constants.ts
@@ -1,10 +1,11 @@
+import Decimal from 'decimal.js'
 import { hexToBigInt } from './index'
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
 
 export const ZERO_BI = 0n
 export const ONE_BI = 1n
-export const ZERO_BD = 0
-export const ONE_BD = 1
+export const ZERO_BD = new Decimal(0)
+export const ONE_BD = new Decimal(1)
 export const Q96 = 2n ** 96n
 export const MaxUint256 = hexToBigInt('ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
