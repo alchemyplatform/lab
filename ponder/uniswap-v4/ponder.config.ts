7f236dbc61ee0dc9323f0735d5fc253398e5d3f2
bastien <9058670+bmoyroud@users.noreply.github.com>
2025-08-07 10:14:07 +0200

    chore: configure config to add more networks in future

--- ponder/uniswap-v4/ponder.config.ts
+++ ponder/uniswap-v4/ponder.config.ts
@@ -5,26 +5,37 @@ import { PositionManagerAbi } from "./abis/PositionManagerAbi";
 import { EulerSwapAbi } from "./abis/EulerSwapAbi";
 
 export default createConfig({
-  chains: { base: { id: 8453, rpc: process.env.PONDER_RPC_URL_8453! } },
+  chains: {
+    base: { id: 8453, rpc: process.env.PONDER_RPC_URL_8453! }
+  },
   contracts: {
     PoolManager: {
-      chain: "base",
-      address: "0x498581fF718922c3f8e6A244956aF099B2652b2b",
       abi: PoolManagerAbi,
-      // startBlock: 25_350_988,
-      startBlock: "latest",
+      chain: {
+        base: {
+          address: "0x498581fF718922c3f8e6A244956aF099B2652b2b",
+          startBlock: 25_350_988,
+          // startBlock: "latest",
+        }
+      }
     },
     PositionManager: {
-      chain: "base",
-      address: "0x0000000000000000000000000000000000000000",
       abi: PositionManagerAbi,
-      startBlock: "latest",
+      chain: {
+        base: {
+          address: "0x7c5f5a4bbd8fd63184577525326123b519429bdc",
+          startBlock: 25_350_993,
+        }
+      }
     },
     EulerSwap: {
-      chain: "base",
-      address: "0xf0CFe22d23699ff1B2CFe6B8f706A6DB63911262",
       abi: EulerSwapAbi,
-      startBlock: 31_658_691,
+      chain: {
+        base: {
+          address: "0xf0CFe22d23699ff1B2CFe6B8f706A6DB63911262",
+          startBlock: 31_658_691,
+        }
+      }
     }
   },
 });
