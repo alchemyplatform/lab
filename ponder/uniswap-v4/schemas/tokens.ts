54fcd29cbaaa65980ea17eafbfce92426539e3d3
bastien <9058670+bmoyroud@users.noreply.github.com>
2025-08-07 11:23:41 +0200

    chore: ensure fields are not null

--- ponder/uniswap-v4/schemas/tokens.ts
+++ ponder/uniswap-v4/schemas/tokens.ts
@@ -8,35 +8,35 @@ export const tokens = onchainTable(
     id: t.text().primaryKey(),
 
     // token symbol
-    symbol: t.text(),
+    symbol: t.text().notNull(),
 
     // token name
-    name: t.text(),
+    name: t.text().notNull(),
 
     // token decimals
     decimals: t.bigint().notNull(),
 
     // token total supply
-    totalSupply: t.bigint(),
+    totalSupply: t.bigint().notNull(),
 
     // volume in token units
-    volume: t.doublePrecision(),
+    volume: t.doublePrecision().notNull(),
 
     // volume in derived USD
-    volumeUSD: t.doublePrecision(),
+    volumeUSD: t.doublePrecision().notNull(),
 
     // volume in USD even on pools with less reliable USD values
-    untrackedVolumeUSD: t.doublePrecision(),
+    untrackedVolumeUSD: t.doublePrecision().notNull(),
 
     // fees in USD
-    feesUSD: t.doublePrecision(),
+    feesUSD: t.doublePrecision().notNull(),
 
     // transactions across all pools that include this token
     txCount: t.bigint().notNull(),
 
     // TODO: use relation instead?
     // number of pools containing this token
-    poolCount: t.bigint(),
+    poolCount: t.bigint().notNull(),
 
     // liquidity across all pools in token units
     totalValueLocked: t.doublePrecision().notNull(),
