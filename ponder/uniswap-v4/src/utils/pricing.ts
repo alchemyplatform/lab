b86907a0fa73c29dbf553f5e60b2341ff259a722
bastien <9058670+bmoyroud@users.noreply.github.com>
2025-08-07 10:48:31 +0200

    chore: add getTrackedAmountUSD helper

--- ponder/uniswap-v4/src/utils/pricing.ts
+++ ponder/uniswap-v4/src/utils/pricing.ts
@@ -2,7 +2,9 @@ import Decimal from "decimal.js"
 import { NativeTokenDetails } from "./nativeTokenDetails"
 import { zeroAddress } from "viem"
 import { exponentToBigDecimal } from ".";
-import { tokens } from "ponder:schema";
+import { bundles, tokens } from "ponder:schema";
+import { ZERO_BD } from "./constants";
+import { Context } from "ponder:registry";
 
 const Q192 = 2n ** 192n;
 
@@ -33,4 +35,45 @@ export function calculateAmountUSD(
   ethPriceUSD: Decimal,
 ): Decimal {
   return amount0.times(token0DerivedETH.times(ethPriceUSD)).plus(amount1.times(token1DerivedETH.times(ethPriceUSD)));
+}
+
+/**
+ * Accepts tokens and amounts, return tracked amount based on token whitelist
+ * If one token on whitelist, return amount in that token converted to USD * 2.
+ * If both are, return sum of two amounts
+ * If neither is, return 0
+ */
+export async function getTrackedAmountUSD(
+  context: Context,
+  tokenAmount0: Decimal,
+  token0: typeof tokens.$inferSelect,
+  tokenAmount1: Decimal,
+  token1: typeof tokens.$inferSelect,
+  whitelistTokens: string[],
+): Promise<Decimal> {
+  const bundle = await context.db.find(bundles, { id: '1' });
+  if (!bundle) {
+    throw new Error('Bundle not found');
+  }
+
+  const price0USD = new Decimal(token0.derivedETH).times(bundle.ethPriceUSD)
+  const price1USD = new Decimal(token1.derivedETH).times(bundle.ethPriceUSD)
+
+  // both are whitelist tokens, return sum of both amounts
+  if (whitelistTokens.includes(token0.id) && whitelistTokens.includes(token1.id)) {
+    return tokenAmount0.times(price0USD).plus(tokenAmount1.times(price1USD))
+  }
+
+  // take double value of the whitelisted token amount
+  if (whitelistTokens.includes(token0.id) && !whitelistTokens.includes(token1.id)) {
+    return tokenAmount0.times(price0USD).times(2)
+  }
+
+  // take double value of the whitelisted token amount
+  if (!whitelistTokens.includes(token0.id) && whitelistTokens.includes(token1.id)) {
+    return tokenAmount1.times(price1USD).times(2)
+  }
+
+  // neither token is on white list, tracked amount is 0
+  return ZERO_BD;
 }
