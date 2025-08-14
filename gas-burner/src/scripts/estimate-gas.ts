ff5740bd7020e713b9d33f86f8231c4f8d12bc7b
bastien <9058670+bmoyroud@users.noreply.github.com>
2025-08-14 22:50:19 +0200

    chore: pass parameter -- gas & format numbers

--- gas-burner/src/scripts/estimate-gas.ts
+++ gas-burner/src/scripts/estimate-gas.ts
@@ -2,6 +2,8 @@ import { createPublicClient, http, parseAbi } from "viem";
 import { botanixTestnet } from "viem/chains";
 import { networkToGasBurnerContractAddress } from "./utils/contract-deployments";
 
+const nf = new Intl.NumberFormat('en-US');
+
 const abi = parseAbi([
   "function burnInternal(uint256 toSpend) returns (uint256 used)",
 ]);
@@ -10,6 +12,28 @@ const publicClient = createPublicClient({ chain: botanixTestnet, transport: http
 
 const network = botanixTestnet;
 
+const args = process.argv.slice(2);
+
+if (args.length !== 1) {
+  throw new Error("Usage: bun run estimate-gas --gas=<target-gas-burn>");
+}
+
+
+if (typeof args[0] !== 'string') {
+  throw new Error('Invalid argument: --gas=<target-gas-burn>');
+}
+
+let gasLimit = args[0].split('--gas=')[1];
+if (!gasLimit) {
+  console.log('No gas limit provided, using default of 10000000');
+  gasLimit = '10000000';
+}
+
+
+const targetGasBurn = BigInt(gasLimit);
+console.log(`Target gas burn: ${nf.format(targetGasBurn)}`);
+
+
 // TODO: Replace with actual contract address once deployed
 const contractAddress = networkToGasBurnerContractAddress.get(network.name) as `0x${string}`;
 
@@ -17,14 +41,21 @@ if (!contractAddress) {
   throw new Error(`No contract address found for ${network.name}`);
 }
 
-// TODO: Replace with gas burn amount
-const TARGET_GAS_BURN = 100_000n;
-
+// 0x60a7e16fe32fe0daaf615d469a6f4e4fcb3774ed
+// 0xc0ec86363f891fdeb3cd50f14628b7a2b71ec08a
 const estimatedGas = await publicClient.estimateContractGas({
   abi,
-  address: contractAddress,
+  address: '0x60a7e16fe32fe0daaf615d469a6f4e4fcb3774ed',
   functionName: "burnInternal",
-  args: [TARGET_GAS_BURN],
+  args: [targetGasBurn],
 });
 
-console.log("Estimated gas:", estimatedGas.toString());
+
+console.table([
+  {
+    Target: nf.format(targetGasBurn),
+    'Estimated Gas': nf.format(estimatedGas),
+    Delta: nf.format(estimatedGas - targetGasBurn),
+    'Delta %': `${((Number(estimatedGas - targetGasBurn) / Number(targetGasBurn)) * 100).toFixed(2)}%`,
+  }
+])
